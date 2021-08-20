2075d5106bdbed031b3d0d660f66a4c6f4e2879b
Annaliskirwa <annalis.kirwa@student.moringaschool.com>
2021-08-21 00:14:28 +0300

    adding a route for product details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }])
+    RouterModule.forRoot([{ path: '', component: ProductListComponent },
+  {path:'products/:productId', component: ProductDetailsComponent}])
   ],
   declarations: [
     AppComponent,
