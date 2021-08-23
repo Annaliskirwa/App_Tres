b30e517806261915047700f4f36ea96b08d9b13b
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-23 20:44:21 +0300

    configuring shipping component to use cart service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { HttpClientModule} from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { HttpClientModule} from '@angular/common/http';
     ReactiveFormsModule,
     RouterModule.forRoot([{ path: '', component: ProductListComponent },
   {path:'products/:productId', component: ProductDetailsComponent},
-  {path: 'cart', component: CartComponent},])
+  {path: 'cart', component: CartComponent},
+  { path:'shipping', component: ShippingComponent},])
   ],
   declarations: [
     AppComponent,
@@ -27,7 +29,8 @@ import { HttpClientModule} from '@angular/common/http';
     ProductAlertsComponent,
     ProductListComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [AppComponent]
 })
