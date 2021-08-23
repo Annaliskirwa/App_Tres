9b3f545138a23afc5e7d12a1291e9dcb903ac664
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-23 20:32:40 +0300

    It will configure the cart service to get the shipping data

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -18,6 +18,10 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+  getShippingPrices(){
+    return this.http.get<{type: string, price: number}[]>
+    ('/assets/shipping.json');
+  }
 
   constructor(private http: HttpClient) { }
 }
