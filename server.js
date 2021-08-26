065ee66c4ddccb3d49a16fbd7df9e2990bde049e
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-26 09:05:39 +0300

    it will change the name of the app/

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SimpleApp'));
+app.use(express.static(__dirname + '/dist/simple-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/SimpleApp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/simple-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
